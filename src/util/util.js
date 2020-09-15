293a34b1e9f03f0a169f9417d7753b198af8b1d9
袁贝尔 <yuanbeier@outlook.com>
2020-09-15 23:59:47 +0800

    新增AES解密方法，临时，目前没啥用

--- src/util/util.js
+++ src/util/util.js
@@ -118,6 +118,33 @@ export const encryption = (params) => {
     return result;
 };
 
+/**
+ * AES 解密 ：字符串 key iv  返回base64
+ *
+ */
+export function Decrypt(word, keyStr, ivStr) {
+    let key  = KEY
+    let iv = IV
+
+    if (keyStr) {
+        key = CryptoJS.enc.Utf8.parse(keyStr);
+        iv = CryptoJS.enc.Utf8.parse(ivStr);
+    }
+
+    let base64 = CryptoJS.enc.Base64.parse(word);
+    let src = CryptoJS.enc.Base64.stringify(base64);
+
+    var decrypt = CryptoJS.AES.decrypt(src, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.ZeroPadding
+    });
+
+    var decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+    return decryptedStr.toString();
+}
+
+
 
 
 
